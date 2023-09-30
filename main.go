0358a7a0b7bb9728aec55dac7d97ae70a858b523
Ed-cred <edwardandrew63@gmail.com>
2023-09-30 11:28:45 +0300

    regex version of normalize

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import "bytes"
+import (
+	"regexp"
+)
 
 func main() {
 }
-
+//regex version
 func normalize(phone string) string {
-	var buf bytes.Buffer
-	for _, ch := range phone {
-		if ch >= '0' && ch <= '9' {
-			buf.WriteRune(ch)	
-		}
-	}
-	return buf.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
 }
+
+//iter version
+// func normalize(phone string) string {
+// 	var buf bytes.Buffer
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			buf.WriteRune(ch)	
+// 		}
+// 	}
+// 	return buf.String()
+// }
