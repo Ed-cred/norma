1d7be3107b08d2c77f176dad52874e28d41bc8bf
Ed-cred <edwardandrew63@gmail.com>
2023-09-30 13:45:59 +0300

    created db connection

--- main.go
+++ main.go
@@ -1,24 +1,74 @@
 package main
 
 import (
-	"regexp"
+	"bytes"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "rootuser"
+	dbname   = "norma"
 )
 
 func main() {
-}
-//regex version
-func normalize(phone string) string {
-	re := regexp.MustCompile("[^0-9]")
-	return re.ReplaceAllString(phone, "")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = resetDB(db, dbname)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Println("couldn't ping the database:", err)
+	}
 }
 
-//iter version
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// iter version
+func normalize(phone string) string {
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		if ch >= '0' && ch <= '9' {
+			buf.WriteRune(ch)
+		}
+	}
+	return buf.String()
+}
+
+// regex version
 // func normalize(phone string) string {
-// 	var buf bytes.Buffer
-// 	for _, ch := range phone {
-// 		if ch >= '0' && ch <= '9' {
-// 			buf.WriteRune(ch)	
-// 		}
-// 	}
-// 	return buf.String()
+// 	re := regexp.MustCompile("[^0-9]")
+// 	return re.ReplaceAllString(phone, "")
 // }
